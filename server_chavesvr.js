2b3730198d92e07ca240b598bb889e63fe3c273b
hnicola77 <henriquenicola@gmail.com>
2025-11-24 07:32:25 -0300

    Update server_chavesvr.js

--- server_chavesvr.js
+++ server_chavesvr.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // ----------------------------------------------------------
 // SQLite – banco principal
 // ----------------------------------------------------------
-const dbPath = "/data";
+const dbPath = "/data/chavesvr.db";
 
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) console.error("❌ Erro ao conectar:", err.message);
@@ -129,4 +129,5 @@ app.delete("/unidades/:id", (req, res) => {
 // ==========================================================
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log("🚀 ChaveVR rodando na porta", PORT));
+
 
